b262a95af54f95b60595368c0c7bfd094255940d
farkuy <darkuy@mail.ru>
2024-10-06 22:39:11 +0300

    Подключил таилвинд до конца. Написал первоночальный конфиг

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,16 @@
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 
 const StartPage = () => {
 
     return (
         <SafeAreaView>
             <ScrollView contentContainerStyle={{height: '100%'}}>
-                <View style={styles.container}>
-                    <Text>Start page</Text>
+                <View className={'flex justify-center items-center h-full w-fulls'}>
+                    <Text className={'flex text-red'}>Start page</Text>
                 </View>
             </ScrollView>
         </SafeAreaView>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100%",
-        width:  "100%",
-    }
-});
 
 export default StartPage;
