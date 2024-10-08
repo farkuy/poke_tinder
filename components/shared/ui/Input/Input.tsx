ef341bf0acdbcee93bd3a3697f3dae7bec041719
farkuy <darkuy@mail.ru>
2024-10-08 16:08:47 +0300

    Добавил закрытие клавиатуры при табе вне инпута

--- components/shared/ui/Input/Input.tsx
+++ components/shared/ui/Input/Input.tsx
@@ -1,6 +1,11 @@
-import { Keyboard, TextInput, TouchableWithoutFeedback } from "react-native";
+import React, { FC } from "react";
+import {
+  Keyboard,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import { TextInputProps } from "react-native/Libraries/Components/TextInput/TextInput";
-import { FC } from "react";
 
 interface InputProps extends TextInputProps {
   loading?: boolean;
@@ -8,17 +13,25 @@ interface InputProps extends TextInputProps {
 }
 
 const Input: FC<InputProps> = (props) => {
-  const { loading, inputStyle, ...anyProps } = props;
+  const { loading, inputStyle = "", ...anyProps } = props;
 
   return (
+    <TextInput
+      textAlign={"center"}
+      className={`w-full bg-gray-200 min-h-[45px] border-4 border-blue-support rounded-xl opacity-80 ${inputStyle}`}
+      {...anyProps}
+    />
+  );
+};
+
+const InputWithDismissKeyboard: FC<InputProps> = (props) => {
+  return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <TextInput
-        textAlign={"center"}
-        className={"w-full bg-red-700"}
-        {...anyProps}
-      />
+      <View className={"flex justify-center items-center w-full h-full"}>
+        <Input {...props} />
+      </View>
     </TouchableWithoutFeedback>
   );
 };
 
-export default Input;
+export default InputWithDismissKeyboard;
